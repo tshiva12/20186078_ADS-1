1fd53ecacc1940f6d522bcc135236331520d7bf8
tshiva12 <shivaprasad233307@msitprogram.net>
2018-09-26 20:17:27 +0530

    "check_style_score -> 0 of 4 passed. style score: 0/1 "

---
+++ m3/3-sum/Solution.java
@@ -0,0 +1,44 @@
+import java.util.Arrays;
+import java.util.Scanner;
+/**
+ * Class for solution of array problem.
+ */
+public final class Solution {
+    /**
+     * Constructs the object of solution.
+     */
+    private Solution() {
+
+    }
+    /**
+     * main function is here.
+     *
+     * @param      args  The arguments
+     */
+    public static void main(final String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int num = scan.nextInt();
+        int[] array = new int[num];
+        for (int i = 0; i < num; i++) {
+            array[i] = scan.nextInt();
+        }
+        Arrays.sort(array);
+        int count = 0;
+        for (int i = 0; i < num - 2; i++) {
+            int j = i + 1;
+            int k = num;
+            while (j < k) {
+                if (array[i] + array[j] + array[k] == 0) {
+                    count++;
+                    j++;
+                    k++;
+                } else if (array[i] + array[j] + array[k] < 0) {
+                    j++;
+                } else {
+                    k++;
+                }
+            }
+        }
+        System.out.println(count);
+    }
+}
