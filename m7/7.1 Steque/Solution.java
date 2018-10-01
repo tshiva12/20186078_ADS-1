bf5492daf3a58b174e14e786aa623491083a71db
tshiva12 <shivaprasad233307@msitprogram.net>
2018-10-01 12:16:42 +0530

    "check_style_score -> 0 of 2 passed. style score: 0/1 "

--- m7/7.1 Steque/Solution.java
+++ m7/7.1 Steque/Solution.java
@@ -6,65 +6,73 @@ import java.util.Scanner;
 // 		this.data = data;
 // 	}
 // }
-// class Steque {
-// 	Node first;
-// 	Node last;
-// 	int size = 0;
-// 	// class Node {
-// 	// 	int data;
-// 	// 	Node next;
-// 	// 	Node(int data) {
-// 	// 		this.data = data;
-// 	// 	}
-// 	// }
-// 	Steque() {
-// 		first = null;
-// 		last = null;
-// 	}
-// 	public void enque(int data) {
-// 		Node oldLast = last;
-// 		last = new Node(data);
-// 		last.data = data;
-// 		last.next = null;
-// 		if (first == null)
-// 			first = last;
-// 		else
-// 			oldLast.next = last;
-// 	}
-// 	public void push(int data) {
-// 		Node oldFirst = first;
-// 		first = new Node(data);
-// 		first.data = data;
-// 		first.next = oldFirst;
-// 		if (last == null) 
-// 			last = first;
-// 	}
-// 	public void pop() {
-// 		if (isEmpty())
-// 			System.out.println("No element exists in Steque");
-// 		data = first.data;
-// 		first = first.next;
-// 		return data;
-// 	}
-// 	public boolean isEmpty() {
-// 		return first == null || last == null;
-// 	}
-// 	// public int size() {
-// 	// 	int count = 0;
-// 	// 	for(int data : data) {
-// 	// 		count++;
-// 	// 	}
-// 	// 	return count;
-// 	// }
-// 	public void show() {
-// 		Node temp = first;
-// 		while (temp.next != null) {
-// 			System.out.print(temp.data + ", ");
-// 			temp = temp.next;
-// 		}
-// 		System.out.println(temp.data);
-// 	}
-// } 
+class Steque {
+	private int data;
+	Node first;
+	Node last;
+	int size = 0;
+
+	class Node {
+	int data;
+	Node next;
+	Node(int data) {
+		this.data = data;
+	}
+}
+	// class Node {
+	// 	int data;
+	// 	Node next;
+	// 	Node(int data) {
+	// 		this.data = data;
+	// 	}
+	// }
+	Steque() {
+		first = null;
+		last = null;
+	}
+	public void enque(int data) {
+		Node oldLast = last;
+		last = new Node(data);
+		last.data = data;
+		last.next = null;
+		if (first == null)
+			first = last;
+		else
+			oldLast.next = last;
+	}
+	public void push(int data) {
+		Node oldFirst = first;
+		first = new Node(data);
+		first.data = data;
+		first.next = oldFirst;
+		if (last == null) 
+			last = first;
+	}
+	public int pop() {
+		// if (isEmpty())
+		// 	System.out.println("No element exists in Steque");
+		data = first.data;
+		first = first.next;
+		return data;
+	}
+	public boolean isEmpty() {
+		return first == null || last == null;
+	}
+	// public int size() {
+	// 	int count = 0;
+	// 	for(int data : data) {
+	// 		count++;
+	// 	}
+	// 	return count;s
+	// }
+public void show() {
+        Node temp = first;
+        while (temp != null) {
+            System.out.print(temp.data + " ");
+            temp = temp.next;
+        }
+    }
+} 
 public class Solution {
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
@@ -93,59 +101,59 @@ public class Solution {
 		}
 	}
 }
-class Steque {
-	Node first = null;
-    Node last = null;
-	int size = 0;
-   	private class Node {
-   		int item;
-        Node next;
-        Node(int item) {
-        	this.item = item;
-        }
-    }      
-    public boolean isEmpty() {
-        return size == 0;
-    }
-	public void push(int item) {
-        Node oldfirst = new Node(item);
-        if (isEmpty()) {
-            first = oldfirst;
-            last = first;
-            last.next = null;
-            size++;
-            return;
-        }
-        Node ofirst = last;
-        last = oldfirst;
-        ofirst.next = last; 
-        last.next = null;
-        size++;
-    }
-   	public int pop() {
-        if (isEmpty()) {
-            return 0;
-        }
-        int item = first.item;
-        first = first.next;
-        size--;
-        return item;
-    }
-    public void show() {
-        Node temp = first;
-        while (temp != null) {
-            System.out.print(temp.item + " ");
-            temp = temp.next;
-        }
-    }
-    public void enque(int data) {
-		Node oldLast = last;
-		last = new Node(data);
-		last.item = data;
-		last.next = null;
-		if (first == null)
-			first = last;
-		else
-			oldLast.next = last;
-	}
-}
+// class Steque {
+// 	Node first = null;
+//     Node last = null;
+// 	int size = 0;
+//    	private class Node {
+//    		int item;
+//         Node next;
+//         Node(int item) {
+//         	this.item = item;
+//         }
+//     }      
+//     public boolean isEmpty() {
+//         return size == 0;
+//     }
+// 	public void push(int item) {
+//         Node oldfirst = new Node(item);
+//         if (isEmpty()) {
+//             first = oldfirst;
+//             last = first;
+//             last.next = null;
+//             size++;
+//             return;
+//         }
+//         Node ofirst = last;
+//         last = oldfirst;
+//         ofirst.next = last; 
+//         last.next = null;
+//         size++;
+//     }
+//    	public int pop() {
+//         if (isEmpty()) {
+//             return 0;
+//         }
+//         int item = first.item;
+//         first = first.next;
+//         size--;
+//         return item;
+//     }
+//     public void show() {
+//         Node temp = first;
+//         while (temp != null) {
+//             System.out.print(temp.item + " ");
+//             temp = temp.next;
+//         }
+//     }
+//     public void enque(int data) {
+// 		Node oldLast = last;
+// 		last = new Node(data);
+// 		last.item = data;
+// 		last.next = null;
+// 		if (first == null)
+// 			first = last;
+// 		else
+// 			oldLast.next = last;
+// 	}
+// }
