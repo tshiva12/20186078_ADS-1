c29dcd3463d14bd6e962cce3ffe369177f3236be
tshiva12 <shivaprasad233307@msitprogram.net>
2018-10-01 15:07:58 +0530

    "check_style_score -> 2 of 2 passed. style score: 0/1 "

--- m7/7.1 Steque/Solution.java
+++ m7/7.1 Steque/Solution.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 /**
  * Class for solution.
  */
-public class Solution {
+public final class Solution {
 	/**
 	 * Constructs the object.
 	 */
@@ -14,20 +14,21 @@ public class Solution {
 	 *
 	 * @param      args  The arguments
 	 */
-	public static void main(String[] args) {
+	public static void main(final String[] args) {
 		Scanner scan = new Scanner(System.in);
 		int n = Integer.parseInt(scan.nextLine());
 		for (int i = 0; i < n; i++) {
 			Steque steque = new Steque();
-			while(scan.hasNext()) {
+			while (scan.hasNext()) {
 				String str = scan.nextLine();
 				if (str.equals("")) {
 					break;					
 				}
 				String[] tokens = str.split(" ");
-				switch(tokens[0]) {
+				switch (tokens[0]) {
 					case "push" :
-					steque.push(Integer.parseInt(tokens[1]));
+					steque.push
+					(Integer.parseInt(tokens[1]));
 					steque.show();
 					break;
 					case "pop" :
@@ -35,7 +36,8 @@ public class Solution {
 					steque.show();
 					break;
 					case "enqueue" :
-					steque.enque(Integer.parseInt(tokens[1]));
+					steque.enque
+					(Integer.parseInt(tokens[1]));
 					steque.show();
 					break;
 					default :
