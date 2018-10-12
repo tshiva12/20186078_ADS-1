ec2ed0060550d1150a23286525f24f0d6889c606
tshiva12 <shivaprasad233307@msitprogram.net>
2018-10-12 17:33:58 +0530

    "taxi_cab_numbers: 0/5 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- m17/Assignment1/TaxiCabNumbers/Solution.java
+++ m17/Assignment1/TaxiCabNumbers/Solution.java
@@ -3,24 +3,61 @@ import java.util.Scanner;
  * Class for cube sum.
  */
 class CubeSum implements Comparable<CubeSum> {
+    /**
+     * Integer variable.
+     */
     private final int sum;
+    /**
+     * Integer variable.
+     */
     private final int i;
+    /**
+     * Integer variable.
+     */
     private final int j;
-    public CubeSum(int i1, int j1) {
+    /**
+     * Constructs the object.
+     *
+     * @param      i1    I 1
+     * @param      j1    The j 1
+     */
+    public CubeSum(final int i1, final int j1) {
         this.sum = i1 * i1 *  i1 + j1 * j1 * j1;
         this.i = i1;
         this.j = j1;
     }
+    /**
+     * Gets the sum.
+     *
+     * @return     The sum.
+     */
     public int getSum() {
         return this.sum;
     }
+    /**
+     * Gets i.
+     *
+     * @return     I.
+     */
     public int getI() {
         return this.i;
     }
+    /**
+     * Gets the j.
+     *
+     * @return     The j.
+     */
     public int getJ() {
         return this.j;
     }
-    public int compareTo(CubeSum that) {
+    /**
+     * { function_description }
+     *
+     * @param      that  The that
+     *
+     * @return     { description_of_the_return_value }
+     */
+    public int compareTo(final CubeSum that) {
         if (this.sum < that.sum) {
             return -1;
         }
@@ -29,19 +66,35 @@ class CubeSum implements Comparable<CubeSum> {
         }
         return 0;
     }
+    /**
+     * Returns a string representation of the object.
+     *
+     * @return     String representation of the object.
+     */
     public String toString() {
         return sum + " = " + i + "^3" + " + " + j + "^3";
     }
 }
-public class Solution {
+/**
+ * Class for solution.
+ */
+public final class Solution {
+    /**
+     * Constructs the object.
+     */
     private Solution() {
         // default constructor is not used.
     }
-    public static void main(String[] args) {
+    /**
+     * main function.
+     *
+     * @param      args  The arguments
+     */
+    public static void main(final String[] args) {
         int n = 1200;
         Scanner scan = new Scanner(System.in);
-        int N = scan.nextInt();
-        int M = scan.nextInt();
+        int num = scan.nextInt();
+        int values = scan.nextInt();
         int count = 0;
         int z = -1;
         MinPQ<CubeSum> pq = new MinPQ<CubeSum>();
@@ -55,9 +108,9 @@ public class Solution {
             } else {
                 count = 0;
             }
-            if (count == M - 1) {
-                N--;
-                if (N == 0) {
+            if (count == values - 1) {
+                values--;
+                if (num == 0) {
                     System.out.println(s.getSum());
                     break;
                 }
