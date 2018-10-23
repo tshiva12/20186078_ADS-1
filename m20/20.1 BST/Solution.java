0cc5fdb2b899f894350c93eafa6231eb378c5104
tshiva12 <shivaprasad233307@msitprogram.net>
2018-10-23 11:42:49 +0530

    ": 3/3 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- m20/20.1 BST/Solution.java
+++ m20/20.1 BST/Solution.java
@@ -261,15 +261,15 @@ class BST {
      * @return     return x value.
      */
     private Node delete(Node x, final Book key) {
-        if (x == null) return null;
-
+        if (x == null) {
+            return null;
+        }
         int cmp = key.compareTo(x.key);
         if (cmp < 0) {
             x.left  = delete(x.left,  key);
-        }
-        else if (cmp > 0) {
+        } else if (cmp > 0) {
             x.right = delete(x.right, key);
-        } else { 
+        } else {
             if (x.right == null) {
                 return x.left;
             }
@@ -280,7 +280,7 @@ class BST {
             x = min(t.right);
             x.right = deleteMin(t.right);
             x.left = t.left;
-        } 
+        }
         x.size = size(x.left) + size(x.right) + 1;
         return x;
     }
