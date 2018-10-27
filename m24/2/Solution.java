f62e0c6c25a179388dc8a3117e1c274eeb8763df
tshiva12 <shivaprasad233307@msitprogram.net>
2018-10-27 13:10:26 +0530

    "m24_a2_1: 0/4 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- m24/2/Solution.java
+++ m24/2/Solution.java
@@ -232,190 +232,190 @@ class SequentialSearchST<Key, Value> {
         return queue;
     }
 }
-// /**
-//  * Class for separate chaining hash st.
-//  *
-//  * @param      <Key>    The key
-//  * @param      <Value>  The value
-//  */
-// class SeparateChainingHashST<Key, Value> {
-//     /**
-//      * Integer variable.
-//      */
-//     private static final int INIT_CAPACITY = 4;
-//     /**
-//      * Integer variable.
-//      */
-//     private int n;
-//     /**
-//      * Integer variable.
-//      */
-//     private int m;
-//     /**
-//      * Sequential variable.
-//      */
-//     private SequentialSearchST<Key, Value>[] st;
-//     /**
-//      * Constructs the object.
-//      */
-//     SeparateChainingHashST() {
-//         this(INIT_CAPACITY);
-//     }
-//     /**
-//      * Constructs the object.
-//      * Time complexity N.
-//      *
-//      * @param      m1    integer variable.
-//      */
-//     SeparateChainingHashST(final int m1) {
-//         this.m = m1;
-//         st = (SequentialSearchST<Key, Value>[]) new SequentialSearchST[m1];
-//         for (int i = 0; i < m1; i++) {
-//             st[i] = new SequentialSearchST<Key, Value>();
-//         }
-//     }
-//     /**
-//      * resize method.
-//      * Time complexity logN.
-//      *
-//      * @param      chains  The chains
-//      */
-//     private void resize(final int chains) {
-//         SeparateChainingHashST<Key, Value>
-//          temp = new SeparateChainingHashST<Key, Value>(chains);
-//         for (int i = 0; i < m; i++) {
-//             for (Key key : st[i].keys()) {
-//                 temp.put(key, st[i].get(key));
-//             }
-//         }
-//         this.m  = temp.m;
-//         this.n  = temp.n;
-//         this.st = temp.st;
-//     }
-//     /**
-//      * Integer variable.
-//      */
-//     private final int z = 0x7fffffff;
-//     /**
-//      * hash function.
-//      *
-//      * @param      key   The key
-//      *
-//      * @return     key variable.
-//      */
-//     private int hash(final Key key) {
-//         return (key.hashCode() & z) % m;
-//     }
-//     /**
-//      * Size.
-//      * Time complexity 1.
-//      *
-//      * @return     return size.
-//      */
-//     public int size() {
-//         return n;
-//     }
-//     /**
-//      * Determines if empty.
-//      *
-//      * @return     True if empty, False otherwise.
-//      */
-//     public boolean isEmpty() {
-//         return size() == 0;
-//     }
-//     /**
-//      * Contains.
-//      * Time complexity logN.
-//      *
-//      * @param      key   The key
-//      *
-//      * @return     return boolean value.
-//      */
-//     public boolean contains(final Key key) {
-//         if (key == null) {
-//             throw new
-//              IllegalArgumentException("argument to contains() is null");
-//         }
-//         return get(key) != null;
-//     }
-//     /**
-//      * Get.
-//      * Time complexity logN.
-//      *
-//      * @param      key   The key
-//      *
-//      * @return     return value.
-//      */
-//     public Value get(final Key key) {
-//         if (key == null) {
-//             throw new IllegalArgumentException("key is null");
-//         }
-//         int i = hash(key);
-//         return st[i].get(key);
-//     }
-//     /**
-//      * Put.
-//      * Time complexity logN.
-//      *
-//      * @param      key   The key
-//      * @param      val   The value
-//      */
-//     public void put(final Key key, final Value val) {
-//         /**
-//          * Integer variable.
-//          */
-//         final int ten = 10;
-//         if (key == null) {
-//             throw new
-//              IllegalArgumentException("first argument to put() is null");
-//         }
-//         if (val == null) {
-//             delete(key);
-//             return;
-//         }
-//         if (n >= ten * m) {
-//             resize(2 * m);
-//         }
-//         int i = hash(key);
-//         if (!st[i].contains(key)) {
-//             n++;
-//         }
-//         st[i].put(key, val);
-//     }
-//     /**
-//      * delete.
-//      * Time complexity logN.
-//      *
-//      * @param      key   The key
-//      */
-//     public void delete(final Key key) {
-//         if (key == null) {
-//             throw new IllegalArgumentException("argument to delete() is null");
-//         }
-//         int i = hash(key);
-//         if (st[i].contains(key)) {
-//             n--;
-//         }
-//         st[i].delete(key);
-//         if (m > INIT_CAPACITY && n <= 2 * m) {
-//             resize(m / 2);
-//         }
-//     }
-//     /**
-//      * Iterable.
-//      *
-//      * @return     return queue.
-//      */
-//     public Iterable<Key> keys() {
-//         Queue<Key> queue = new Queue<Key>();
-//         for (int i = 0; i < m; i++) {
-//             for (Key key : st[i].keys()) {
-//                 queue.enqueue(key);
-//             }
-//         }
-//         return queue;
-//     }
-// }
+/**
+ * Class for separate chaining hash st.
+ *
+ * @param      <Key>    The key
+ * @param      <Value>  The value
+ */
+class SeparateChainingHashST<Key, Value> {
+    /**
+     * Integer variable.
+     */
+    private static final int INIT_CAPACITY = 4;
+    /**
+     * Integer variable.
+     */
+    private int n;
+    /**
+     * Integer variable.
+     */
+    private int m;
+    /**
+     * Sequential variable.
+     */
+    private SequentialSearchST<Key, Value>[] st;
+    /**
+     * Constructs the object.
+     */
+    SeparateChainingHashST() {
+        this(INIT_CAPACITY);
+    }
+    /**
+     * Constructs the object.
+     * Time complexity N.
+     *
+     * @param      m1    integer variable.
+     */
+    SeparateChainingHashST(final int m1) {
+        this.m = m1;
+        st = (SequentialSearchST<Key, Value>[]) new SequentialSearchST[m1];
+        for (int i = 0; i < m1; i++) {
+            st[i] = new SequentialSearchST<Key, Value>();
+        }
+    }
+    /**
+     * resize method.
+     * Time complexity logN.
+     *
+     * @param      chains  The chains
+     */
+    private void resize(final int chains) {
+        SeparateChainingHashST<Key, Value>
+         temp = new SeparateChainingHashST<Key, Value>(chains);
+        for (int i = 0; i < m; i++) {
+            for (Key key : st[i].keys()) {
+                temp.put(key, st[i].get(key));
+            }
+        }
+        this.m  = temp.m;
+        this.n  = temp.n;
+        this.st = temp.st;
+    }
+    /**
+     * Integer variable.
+     */
+    private final int z = 0x7fffffff;
+    /**
+     * hash function.
+     *
+     * @param      key   The key
+     *
+     * @return     key variable.
+     */
+    private int hash(final Key key) {
+        return (key.hashCode() & z) % m;
+    }
+    /**
+     * Size.
+     * Time complexity 1.
+     *
+     * @return     return size.
+     */
+    public int size() {
+        return n;
+    }
+    /**
+     * Determines if empty.
+     *
+     * @return     True if empty, False otherwise.
+     */
+    public boolean isEmpty() {
+        return size() == 0;
+    }
+    /**
+     * Contains.
+     * Time complexity logN.
+     *
+     * @param      key   The key
+     *
+     * @return     return boolean value.
+     */
+    public boolean contains(final Key key) {
+        if (key == null) {
+            throw new
+             IllegalArgumentException("argument to contains() is null");
+        }
+        return get(key) != null;
+    }
+    /**
+     * Get.
+     * Time complexity logN.
+     *
+     * @param      key   The key
+     *
+     * @return     return value.
+     */
+    public Value get(final Key key) {
+        if (key == null) {
+            throw new IllegalArgumentException("key is null");
+        }
+        int i = hash(key);
+        return st[i].get(key);
+    }
+    /**
+     * Put.
+     * Time complexity logN.
+     *
+     * @param      key   The key
+     * @param      val   The value
+     */
+    public void put(final Key key, final Value val) {
+        /**
+         * Integer variable.
+         */
+        final int ten = 10;
+        if (key == null) {
+            throw new
+             IllegalArgumentException("first argument to put() is null");
+        }
+        if (val == null) {
+            delete(key);
+            return;
+        }
+        if (n >= ten * m) {
+            resize(2 * m);
+        }
+        int i = hash(key);
+        if (!st[i].contains(key)) {
+            n++;
+        }
+        st[i].put(key, val);
+    }
+    /**
+     * delete.
+     * Time complexity logN.
+     *
+     * @param      key   The key
+     */
+    public void delete(final Key key) {
+        if (key == null) {
+            throw new IllegalArgumentException("argument to delete() is null");
+        }
+        int i = hash(key);
+        if (st[i].contains(key)) {
+            n--;
+        }
+        st[i].delete(key);
+        if (m > INIT_CAPACITY && n <= 2 * m) {
+            resize(m / 2);
+        }
+    }
+    /**
+     * Iterable.
+     *
+     * @return     return queue.
+     */
+    public Iterable<Key> keys() {
+        Queue<Key> queue = new Queue<Key>();
+        for (int i = 0; i < m; i++) {
+            for (Key key : st[i].keys()) {
+                queue.enqueue(key);
+            }
+        }
+        return queue;
+    }
+}
 /**
  * Class for solution.
  */
@@ -433,8 +433,8 @@ public final class Solution {
      */
     public static void main(final String[] args) {
         Scanner scan = new Scanner(System.in);
-        SequentialSearchST<StudentData, Integer>
-         hash = new SequentialSearchST<StudentData, Integer>();
+        SeparateChainingHashST<StudentData, Integer>
+         hash = new SeparateChainingHashST<StudentData, Integer>();
         int n = Integer.parseInt(scan.nextLine());
         for (int i = 1; i <= n; i++) {
             String[] tokens = scan.nextLine().split(",");
